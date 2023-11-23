e9c14e2a9f6e4730d015ded5299451a30851cb00
Janos Sitku <sitku.janos86@gmail.com>
2023-11-23 15:56:46 +0100

    some done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,19 +1,52 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(number1, number2) {
+  if(number1 >= number2) {
+    return number1;
+  }
+  else {
+    return number2;
+  }
+}
 
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(wordArray) {
+  if(wordArray.length === 0) {
+    return null;
+  }
+  let longestWord = wordArray[0]; 
+
+  for (let i = 0; i < wordArray.length; i += 1) {
+    if (wordArray[i].length > longestWord.length) {
+      longestWord = wordArray[i];
+    }
+  }
+
+  return longestWord;
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbersArray) {
+  if(numbersArray.length === 0) {
+    return 0;
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < numbersArray.length; i++) {
+    sum += numbersArray[i];
+  }
+
+  return sum;
+}
+
 
 
 
